a13fe68592b4ac667a566e008cb691e07f9fb7be
Thomas Anciaux <zebigbozz@gmail.com>
2014-06-26 09:19:42 +0200

    Update Gruntfile.js
    Removed unused package references & config

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,33 +7,6 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
       clean: [TARGET],
-      /*requirejs: {
-          development: {
-              // Options: https://github.com/jrburke/r.js/blob/master/build/example.build.js
-              options: {
-                  baseUrl: SRC,
-                  dir: TARGET,
-                  optimize: "none",
-                  keepBuildDir: true,
-                  skipDirOptimize: true,
-                  preserveLicenseComments: true,
-                  fileExclusionRegExp: /^\.|\.ts|\.map/
-              }
-          },
-          release: {
-              options: {
-                  name: SRC + "/Weez",
-                  out: DIST + "/Weez.js",
-                  optimize: "none",
-                  keepBuildDir: true,
-                  skipDirOptimize: true,
-                  removeCombined: true,
-                  preserveLicenseComments: true,
-                  fileExclusionRegExp: /^\.|\.ts|\.map/
-              }
-
-          }
-      },*/
       typescript: {
           development: {
               files: [
@@ -64,7 +37,6 @@ module.exports = function(grunt) {
 
     // load in grunt tasks
     grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-jasmine');
     grunt.loadNpmTasks('grunt-typescript');
     grunt.loadNpmTasks('grunt-jasmine-node');
 
